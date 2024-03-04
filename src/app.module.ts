e216f45a0e88d66bfcbe8999ed4af08e5bc42ea0
selimolmaz <selimolmazz@gmail.com>
2024-03-04 15:52:19 +0300

    Fix file name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './User1/User.module';
+import { UserModule } from './User/User.module';
 
 
 @Module({
